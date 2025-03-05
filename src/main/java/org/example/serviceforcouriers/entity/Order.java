46fe625e5eae9b1df9b16728c3b7225955e1c49b
Zaraza-it <egor76522@Inbox.ri>
2025-03-05 19:48:23 +0300

    2

--- src/main/java/org/example/serviceforcouriers/entity/Order.java
+++ src/main/java/org/example/serviceforcouriers/entity/Order.java
@@ -1,5 +1,13 @@
 package org.example.serviceforcouriers.entity;
 
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 public class Order {
 
     private Long id;
@@ -18,93 +26,7 @@ public class Order {
 
     private boolean soldStatus;
 
-    public Order(String product, String customerName, String executorName, String address, Character purchasesPrice, Character purchasesSell, boolean soldStatus) {
-        this.product = product;
-        this.customerName = customerName;
-        this.executorName = executorName;
-        this.address = address;
-        this.purchasesPrice = purchasesPrice;
-        this.purchasesSell = purchasesSell;
-        this.soldStatus = soldStatus;
-    }
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getProduct() {
-        return product;
-    }
-
-    public void setProduct(String product) {
-        this.product = product;
-    }
-
-    public String getCustomerName() {
-        return customerName;
-    }
-
-    public void setCustomerName(String customerName) {
-        this.customerName = customerName;
-    }
-
-    public String getExecutorName() {
-        return executorName;
-    }
-
-    public void setExecutorName(String executorName) {
-        this.executorName = executorName;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    public Character getPurchasesPrice() {
-        return purchasesPrice;
-    }
-
-    public void setPurchasesPrice(Character purchasesPrice) {
-        this.purchasesPrice = purchasesPrice;
-    }
-
-    public Character getPurchasesSell() {
-        return purchasesSell;
-    }
-
-    public void setPurchasesSell(Character purchasesSell) {
-        this.purchasesSell = purchasesSell;
-    }
-
-    public boolean isSoldStatus() {
-        return soldStatus;
-    }
-
-    public void setSoldStatus(boolean soldStatus) {
-        this.soldStatus = soldStatus;
-    }
-
-    @Override
-    public String toString() {
-        return "Order{" +
-                "id=" + id +
-                ", product='" + product + '\'' +
-                ", customerName='" + customerName + '\'' +
-                ", executorName='" + executorName + '\'' +
-                ", address='" + address + '\'' +
-                ", purchasesPrice=" + purchasesPrice +
-                ", purchasesSell=" + purchasesSell +
-                ", soldStatus=" + soldStatus +
-                '}';
-    }
 }
 
 
