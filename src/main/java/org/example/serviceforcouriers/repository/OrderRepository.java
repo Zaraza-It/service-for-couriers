ec0af4aac25636882dbf68d6c3dfb81443b6e143
flowihs <zmulenko500@gmail.com>
2025-03-05 20:40:59 +0300

    repository

--- src/main/java/org/example/serviceforcouriers/repository/OrderRepository.java
+++ src/main/java/org/example/serviceforcouriers/repository/OrderRepository.java
@@ -1,5 +1,12 @@
 package org.example.serviceforcouriers.repository;
 
-public interface OrderRepository {
+import org.aspectj.weaver.ast.Or;
+import org.example.serviceforcouriers.entity.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.cdi.JpaRepositoryExtension;
+import org.springframework.stereotype.Repository;
+
+public interface OrderRepository extends JpaRepository<Order, Long> {
+
 
 }
