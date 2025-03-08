987b54d97bfe04bee56064ada662967126af0b80
Zaraza-it <egor76522@Inbox.ri>
2025-03-09 00:23:51 +0300

    added the gateway configuration for service-for-couriers

--- gateway/src/main/java/org/example/gateway/config/FilterSupplier.java
+++
@@ -1,8 +0,0 @@
-package org.example.gateway.config;
-
-import com.fasterxml.jackson.databind.ser.impl.SimpleFilterProvider;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-public class FilterSupplier {
-}
--- gateway/src/main/java/org/example/gateway/config/GatewayRoutes.java
+++ gateway/src/main/java/org/example/gateway/config/GatewayRoutes.java
@@ -4,6 +4,8 @@ import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import reactor.core.publisher.Mono;
 
 @Configuration
 public class GatewayRoutes {
@@ -14,12 +16,27 @@ public RouteLocator setStatusRoute(RouteLocatorBuilder builder) {
         return builder.routes()
                 .route("setStatusRoute",r ->
                         r.path("/products/{productId}/status")
-                                .filters(f -> f.modifyRequestBody())
-                                .uri("lb://http//localhost:8080"))
+                                .filters(f -> f.rewritePath("(?<productId>.*)", "${productId}"))
+                                .uri("http://localhost:8080"))
                 .build();
     }
 
+@Bean
+public RouteLocator getOrderById(RouteLocatorBuilder builder) {
+    return builder.routes()
+            .route("getOrderByIdRoute",r ->
+                    r.path("/products/{productId}")
+                            .filters(f -> f.rewritePath("(?<productId>.*)","${productId}"))
+                            .uri("http://localhost:8080")).build();
+}
 
 
+@Bean
+public RouteLocator getAllOrders(RouteLocatorBuilder builder) {
+    return builder.routes()
+            .route("getAllOrdersRoute",r ->
+                    r.path("/products")
+                            .uri("http://localhost:8080")).build();
+}
 
 }
