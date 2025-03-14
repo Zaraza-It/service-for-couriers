2b0bb476bf25b4c5aff9adbec2021103f79e90d1
Zaraza-it <egor76522@Inbox.ri>
2025-03-14 21:06:22 +0300

    add new Global Filter Authorization

--- gateway/src/main/java/org/example/gateway/filters/AuthGlobalFilter.java
+++ gateway/src/main/java/org/example/gateway/filters/AuthGlobalFilter.java
@@ -1,23 +1,53 @@
 package org.example.gateway.filters;
 
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.example.gateway.response.TokenResponse;
+import org.example.gateway.service.JwtService;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.core.Ordered;
+import org.springframework.http.server.ServerHttpResponse;
+import org.springframework.http.server.ServletServerHttpResponse;
+import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 
+import java.net.URI;
+@Component
+@RequiredArgsConstructor
 public class AuthGlobalFilter implements GlobalFilter, Ordered {
 
-
-
+    public final JwtService jwtService;
+    public static final String BEARER_PREFIX = "Bearer ";
+    @Value("${location.filter.auth.url}") String url;
 
     @Override
     public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
         if (exchange.getRequest().getHeaders() != null) {
+            if (exchange.getRequest().getHeaders().get("RefreshToken") != null) {
+        String refreshToken = exchange.getRequest().getHeaders().getFirst("RefreshToken");
+            if (jwtService.validateRefreshToken(refreshToken) == true) {
+                String accessToken = exchange.getRequest().getHeaders().getFirst(BEARER_PREFIX);
+                if (accessToken != null) {
+                 accessToken.substring(BEARER_PREFIX.length());
+                    if (jwtService.validateAccessToken(accessToken) == true) {
+                        return chain.filter(exchange);
+                    }
+                    else if (jwtService.validateRefreshToken(refreshToken) == true) {
+                        TokenResponse responseToken = jwtService.updateAccessTokenByRefreshToken(refreshToken);
+                        exchange.getResponse().getHeaders().set("RefreshToken", accessToken);
+                        exchange.getResponse().getHeaders().set("AccessToken", responseToken.getAccessToken());
+                        return chain.filter(exchange);
+                    }
+                    else  exchange.getResponse().getHeaders().setLocation(URI.create(url));
+                }
+            }
 
 
-            return chain.filter(exchange);
+            }
         }
 
         return null;
@@ -25,6 +55,6 @@ public class AuthGlobalFilter implements GlobalFilter, Ordered {
 
     @Override
     public int getOrder() {
-        return 0;
+        return -1;
     }
 }
--- gateway/src/main/java/org/example/gateway/service/JwtService.java
+++ gateway/src/main/java/org/example/gateway/service/JwtService.java
@@ -3,6 +3,7 @@ package org.example.gateway.service;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.ExpiredJwtException;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 import lombok.RequiredArgsConstructor;
@@ -13,9 +14,12 @@ import org.example.gateway.model.User;
 import org.example.gateway.response.TokenResponse;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.lang.NonNull;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 
 import java.security.Key;
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
 
 @Component
 @RequiredArgsConstructor
@@ -71,12 +75,24 @@ public boolean validateRefreshToken(@NonNull String token) {
         if (validateRefreshToken(refreshToken)) {
             final Claims claims = getRefreshClaims(refreshToken);
             final String username = claims.getSubject();
-            if (userService.getByUsername(username) != null) {
+            final User user = userService.getByUsername(username);
+            if (user != null) {
                 final String accessToken = generateAccessToken(user);
                 return new TokenResponse(accessToken, null);
             }
         }
         return new TokenResponse(null, null);
     }
-
+    public String generateAccessToken(UserDetails userDetails) {
+        if (userDetails instanceof User customUD) {
+            return Jwts.builder()
+                    .setSubject(customUD.getUsername())
+                    .setExpiration(new Date(System.currentTimeMillis() + TimeUnit.MINUTES.toMillis(5)))
+                    .signWith(getSigningKey(), SignatureAlgorithm.HS256)
+                    .claim("username", customUD.getUsername())
+                    .claim("roles", customUD.getRoles())
+                    .compact();
+        }
+        return null;
+    }
 }
