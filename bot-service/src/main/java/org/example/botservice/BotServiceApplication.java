419a4cc93816b915cca1120d6c09f4dc72640666
AesaQ <ilyaovchinnikov2095@gmail.com>
2025-04-03 17:13:29 +0500

    refactoring

--- bot-service/src/main/java/org/example/botservice/BotServiceApplication.java
+++ bot-service/src/main/java/org/example/botservice/BotServiceApplication.java
@@ -1,27 +1,11 @@
     package org.example.botservice;
 
-import org.aspectj.weaver.ast.Or;
-import org.example.botservice.dao.OrderRepository;
-import org.example.botservice.dto.Order;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.domain.Example;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
-import org.springframework.data.repository.query.FluentQuery;
-import org.telegram.telegrambots.longpolling.TelegramBotsLongPollingApplication;
-import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
-
-import java.util.List;
-import java.util.Optional;
-import java.util.function.Function;
 
     @SpringBootApplication
 public class BotServiceApplication {
-
         public static void main(String[] args) {
             SpringApplication.run(BotServiceApplication.class, args);
-
         }
     }
--- bot-service/src/main/java/org/example/botservice/dto/Order.java
+++ bot-service/src/main/java/org/example/botservice/dto/Order.java
@@ -3,44 +3,49 @@ package org.example.botservice.dto;
 
 import jakarta.annotation.Nullable;
 import jakarta.persistence.*;
-import jakarta.validation.constraints.Max;
 import lombok.Builder;
 import lombok.Data;
 
-import java.io.Serializable;
 import java.math.BigDecimal;
 import java.time.OffsetDateTime;
 import java.util.UUID;
 
 @Entity
-@Table(name = "orders")
 @Data
 @Builder
+@Table(name = "orders")
 public class Order {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "order_id")
     private Long orderId;
+
     @Column(name = "product")
     private String product;
 
+    @Column(name = "customer_name")
     private String customerName;
 
-    @Column(name = "callback")
     @Nullable
+    @Column(name = "callback")
     private UUID callback;
 
+    @Column(name = "executor_name")
     private String executorName;
 
+    @Column(name = "address")
     private String address;
 
-    private OffsetDateTime offsetDateTime;
+    @Column(name = "offset_date_time")
+    private OffsetDateTime offsetDateTime; //название не понятное, непонятно за что это время отвечает
 
+    @Column(name = "purchases_price")
     private BigDecimal purchasesPrice;
 
+    @Column(name = "purchases_sell")
     private BigDecimal purchasesSell;
 
+    @Column(name = "sold_status")
     private String soldStatus;
-
 }
